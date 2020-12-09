ffea330818433dbb81308f1c7217bd7da92f3074
Adarsh pandya <adarshpandya0@gmail.com>
2020-12-09 01:34:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// @mark - 1 
+
+// @requiring needed dependencies
+const readlineSync = require('readline-sync');
+const chalk = require('chalk')
+
+console.log(chalk.bgBlackBright.green('hello random user'));
